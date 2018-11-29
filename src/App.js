4a18e92a3d8b8d04b0af89f88ff74e1626412548
Robert Silver <rsilver.dev@gmail.com>
2018-11-29 14:22:15 -0500

    Adds page navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+
+import { Route, Switch } from 'react-router';
+
 import './App.css';
+
+import Index from './pages/index';
+
 
 class App extends Component {
   render() {
+
+    var pagesView = (
+      <Switch>
+        <Route exact path="/" component={Index}></Route>
+      </Switch>
+    );
+
     return (
       <div className="App">
         <header className="App-header">
-          Application
+          {pagesView}
         </header>
       </div>
     );
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+    render() {
+        return(
+            <div style={{ width:"100%" }}>
+                Page
+            </div>
+        );
+    }
+}
+
+export default Index;
